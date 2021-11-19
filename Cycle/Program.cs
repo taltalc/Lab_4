b2207507110378a4d20de673d25391c582843253
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-19 15:24:26 +0500

    Добавьте файлы проекта.

---
+++ Cycle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            int Sum = 0;
+                
+           if(number>0)
+            {
+                for (int count = 1; count <= number; count++)
+                {
+
+                    Sum += (2 * count - 1);
+                    Console.WriteLine(Sum);
+                }
+            }
+           else
+            {
+                Console.WriteLine("Ошибка!");
+            }
+              Console.ReadKey();  
+            }
+            
+        }
+    }
+
